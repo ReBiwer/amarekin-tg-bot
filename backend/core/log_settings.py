ea19a653624ec6a9493b2cb4899f05488530d15a
vladimir <vovka1998@gmail.com>
2025-06-11 19:13:48 +0300

    добавил настройки логирования

---
+++ backend/core/log_settings.py
@@ -0,0 +1,52 @@
+import os
+import logging
+import logging.config
+
+from backend.core.config import settings
+
+LOG_DIR = settings.BASE_DIR / "logs"
+
+
+def setup_logging() -> None:
+    os.makedirs(LOG_DIR, exist_ok=True)
+
+    logging_config = {
+        "version": 1,
+        "disable_existing_loggers": False,
+        "formatters": {
+            "default": {
+                "format": "[%(asctime)s] [%(levelname)s] %(name)s:%(lineno)d - %(message)s",
+                "datefmt": "%Y-%m-%d %H:%M:%S",
+            },
+            "console": {
+                "()": "colorlog.ColoredFormatter",
+                "format": "%(log_color)s[%(asctime)s] [%(levelname)s] %(name)s:%(lineno)d - %(message)s",
+                "log_colors": {
+                    "DEBUG": "cyan",
+                    "INFO": "green",
+                    "WARNING": "yellow",
+                    "ERROR": "red",
+                    "CRITICAL": "bold_red",
+                },
+            },
+        },
+        "filters": {
+            "user_websocket": {"()": "logging.Filter", "name": "user_websocket"},
+        },
+        "handlers": {
+            "console": {
+                "class": "logging.StreamHandler",
+                "level": "DEBUG",
+                "formatter": "console",
+                "stream": "ext://sys.stdout",
+            },
+        },
+        "loggers": {
+            settings.NAME_LOGGER: {
+                "level": "DEBUG",
+                "handlers": ["console"],
+            },
+        },
+    }
+
+    logging.config.dictConfig(logging_config)

f7e1a9d82043f5f12440c5271b3a88d6b8c44693
vladimir <vovka1998@gmail.com>
2025-06-16 15:56:35 +0300

    Изменил запуск бекенда

--- backend/main.py
+++ backend/main.py
@@ -1,19 +1,36 @@
-from dotenv import load_dotenv
-load_dotenv()
-
+import uvicorn
 from fastapi import FastAPI
+from contextlib import asynccontextmanager, AbstractAsyncContextManager
+
 from backend.api.v1.endpoints import users, ai
+from backend.di import init_di_web
+from backend.core.log_settings import setup_logging
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI) -> AbstractAsyncContextManager[None]:
+    setup_logging()
+    init_di_web(app)
+    yield
+    await app.state.dishka_container.close()
 
 app = FastAPI(
     title="Telegram AI Assistant Backend",
     description="Backend for handling AI requests from Telegram bot."
 )
 
-app.include_router(ai.router, prefix="/api/v1", tags=["AI"])
-app.include_router(users.router, prefix="/api/v1/users", tags=["Users"])
+app.include_router(ai.router)
+app.include_router(users.router)
 
-@app.get("/health")
-async def health_check():
-    return {"status": "ok", "message": "Backend is running!"}
 
-# В будущем здесь будут подключаться другие маршруты из backend/app/api/ 
+def create_app() -> FastAPI:
+    app = FastAPI(
+        title="Telegram AI Assistant Backend",
+        description="Backend for handling AI requests from Telegram bot.",
+        lifespan=lifespan,
+    )
+    return app
+
+
+if __name__ == "__main__":
+    uvicorn.run("web:create_app", port=8000, factory=True, reload=True)

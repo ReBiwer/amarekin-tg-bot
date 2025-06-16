598dfabdba30594e3c3d23b3996e7bc68e571a97
vladimir <vovka1998@gmail.com>
2025-06-16 16:11:12 +0300

    Обновил конфиг бота

--- bot/config.py
+++ bot/config.py
@@ -1,13 +1,17 @@
+from pathlib import Path
+
 from pydantic import SecretStr
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
+    BASE_DIR: Path = Path(__file__).resolve().parent.parent
     TELEGRAM_BOT_TOKEN: SecretStr = "YOUR_TELEGRAM_BOT_TOKEN"
     BACKEND_URL: str = "http://localhost:8000"
     DATABASE_URL: str = "sqlite:///./sql_app.db"
 
-    model_config = SettingsConfigDict(env_file=".env", extra="ignore")
+    model_config = SettingsConfigDict(env_file=f"/{BASE_DIR}/.env", extra="ignore")
 
 
-settings = Settings() 
+settings = Settings()
+print(settings.BASE_DIR)

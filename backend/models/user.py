98ca1f5606f5527880f8f3f91c1a6f6db16b843f
vladimir <vovka1998@gmail.com>
2025-06-17 08:23:27 +0300

    Уменьшил поля в модели пользователя

--- backend/models/user.py
+++ backend/models/user.py
@@ -5,8 +5,5 @@ from backend.db.database import Base
 
 class User(Base):
     username: Mapped[str]
-    password: Mapped[str]
     telegram_id: Mapped[int] = mapped_column(Integer, unique=True, index=True)
     first_name: Mapped[str | None]
-    last_name: Mapped[str | None]
-    email: Mapped[str | None]

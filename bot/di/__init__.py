dc5feaa0ffad7d8087a0af753decfbb489bfc741
vladimir <vovka1998@gmail.com>
2025-06-18 21:14:25 +0300

    Добавил провайдер для AIService

--- bot/di/__init__.py
+++ bot/di/__init__.py
@@ -7,13 +7,15 @@ from dishka import make_async_container, AsyncContainer
 
 from bot.di.auth import AuthProvider
 from bot.di.logger_di import LoggerProvider
+from bot.di.ai_service import AIProvider
 
 
 def container_factory() -> AsyncContainer:
     return make_async_container(
         AiogramProvider(), # для получения объектов AiogramMiddlewareData и TelegramObject в провайдерах
         AuthProvider(),
-        LoggerProvider()
+        LoggerProvider(),
+        AIProvider()
     )
 
 def init_di_bot(dp: Dispatcher) -> None:

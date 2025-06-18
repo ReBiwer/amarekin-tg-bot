6926b7b44e89c7637e509af8ee6ff8b15308baed
vladimir <vovka1998@gmail.com>
2025-06-18 12:11:14 +0300

    Добавил setup logging при создании бота

--- bot/main.py
+++ bot/main.py
@@ -5,6 +5,7 @@ from aiogram.types import BotCommand, BotCommandScopeDefault
 from bot.config import settings
 from bot.di import init_di_bot
 from bot.dialogs import main_router
+from bot.log_settings import setup_logging
 
 
 async def set_commands(bot: Bot):
@@ -20,6 +21,7 @@ async def main():
     dp = Dispatcher()
     dp.include_router(main_router)
     init_di_bot(dp)
+    setup_logging()
     await dp.start_polling(bot)
 
 if __name__ == "__main__":

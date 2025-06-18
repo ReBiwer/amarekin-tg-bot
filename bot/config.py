4f7f05c014ac4b639ad87ac1fda2755c298382cc
vladimir <vovka1998@gmail.com>
2025-06-18 12:09:33 +0300

    добавил настройки логирования

--- bot/config.py
+++ bot/config.py
@@ -7,11 +7,10 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     BASE_DIR: Path = Path(__file__).resolve().parent.parent
     TELEGRAM_BOT_TOKEN: SecretStr = "YOUR_TELEGRAM_BOT_TOKEN"
-    BACKEND_URL: str = "http://localhost:8000"
-    DATABASE_URL: str = "sqlite:///./sql_app.db"
+    BACKEND_URL: str = "http://localhost:8080"
+    NAME_LOGGER: str = "tg_bot"
 
     model_config = SettingsConfigDict(env_file=f"/{BASE_DIR}/.env", extra="ignore")
 
 
 settings = Settings()
-print(settings.BASE_DIR)
---
+++ bot/log_settings.py
@@ -0,0 +1,49 @@
+import os
+import logging
+import logging.config
+
+from bot.config import settings
+
+LOG_DIR = settings.BASE_DIR / "logs/tg_bot"
+
+
+def setup_logging() -> None:
+    os.makedirs(LOG_DIR, exist_ok=True)
+
+    logging_config = {
+        "version": 1,
+        "disable_existing_loggers": False,
+        "formatters": {
+            "default": {
+                "format": "[%(asctime)s] [%(levelname)s] %(name)s:%(lineno)d - %(message)s",
+                "datefmt": "%Y-%m-%d %H:%M:%S",
+            },
+            "console": {
+                "()": "colorlog.ColoredFormatter",
+                "format": "%(log_color)s[%(asctime)s] [%(levelname)s] %(name)s:%(lineno)d - %(message)s",
+                "log_colors": {
+                    "DEBUG": "cyan",
+                    "INFO": "green",
+                    "WARNING": "yellow",
+                    "ERROR": "red",
+                    "CRITICAL": "bold_red",
+                },
+            },
+        },
+        "handlers": {
+            "console": {
+                "class": "logging.StreamHandler",
+                "level": "DEBUG",
+                "formatter": "console",
+                "stream": "ext://sys.stdout",
+            },
+        },
+        "loggers": {
+            settings.NAME_LOGGER: {
+                "level": "DEBUG",
+                "handlers": ["console"],
+            },
+        },
+    }
+
+    logging.config.dictConfig(logging_config)

d53e21a8b877fdfd5b94da2e7ec032cc811bf417
vladimir <vovka1998@gmail.com>
2025-06-18 21:14:56 +0300

    Добавил роутер к диспетчеру бота

--- bot/dialogs/__init__.py
+++ bot/dialogs/__init__.py
@@ -1,9 +1,11 @@
 from aiogram.dispatcher.router import Router
 
 from bot.dialogs.start import router as start_router
+from bot.dialogs.user_text import router as user_text
 
 main_router = Router()
 
 main_router.include_routers(
-    start_router
+    start_router,
+    user_text
 )
